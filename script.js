d0ec5e79d36942481c80e9310626a99019d99904
IderaNimah <iderayusuf29@gmail.com>
2023-07-19 16:12:19 +0100

    functionality ongoing

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const billAmountInput = document.querySelector(".bill_input");
 const numPeopleInput = document.querySelector(".people_input");
-
 const tipAmount = document.querySelector(".tip_value");
 const totalPersonCalculaor = document.querySelector(".total_person_calc");
 
-const billAmount = billAmountInput.Value;
-console.log(billAmountInput);
+billAmountInput.value = "0.0";
+numPeopleInput.value = "1";
+tipAmount.innerHTML = "$" + (0.0).toFixed(2);
+totalPersonCalculaor.innerHTML = "$" + (0.0).toFixed(2);
+
