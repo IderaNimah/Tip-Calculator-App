6edb3a5a0af1c5eb31d9cb560468b04154e7cc81
IderaNimah <iderayusuf29@gmail.com>
2023-07-19 16:23:14 +0100

    functionality

--- script.js
+++ script.js
@@ -3,8 +3,27 @@ const numPeopleInput = document.querySelector(".people_input");
 const tipAmount = document.querySelector(".tip_value");
 const totalPersonCalculaor = document.querySelector(".total_person_calc");
 
+
+billAmountInput.addEventListener("input", billInputFun);
+numPeopleInput.addEventListener("input", peopleInputFun);
+
+
 billAmountInput.value = "0.0";
 numPeopleInput.value = "1";
 tipAmount.innerHTML = "$" + (0.0).toFixed(2);
 totalPersonCalculaor.innerHTML = "$" + (0.0).toFixed(2);
+
+let billValue = 0.0;
+let peopleValue = 1;
+
+function billInputFun(){
+    billValue=parseFloat(billAmountInput.value)
+    console.log(billValue);
+}
+
+function peopleInputFun(){
+    peopleValue=parseFloat(numPeopleInput.value)
+    console.log(peopleValue);
+}
+
 
